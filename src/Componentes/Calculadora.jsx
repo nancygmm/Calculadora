2bdb3b35a038fcf497712b1a695aa8963feb6d55
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-05-18 21:31:30 -0600

    Buttons and result

--- src/Componentes/Calculadora.jsx
+++ src/Componentes/Calculadora.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './calculadora.css'
+import Boton from './Boton'
 
 const Calculadora = () => {
   return (
     <main>
-        <input className='answer' type="text" />
+        <span className='answer'>50</span>
+        <Boton texto='C' clase='gris' />
+        <Boton texto='+/-' clase='gris' />
+        <Boton texto='%' clase='gris' />
+        <Boton texto='/' clase='operacion' />
+        <Boton texto='7' clase='numero' />
+        <Boton texto='8' clase='numero' />
+        <Boton texto='9' clase='numero' />
+        <Boton texto='*' clase='operacion' />
+        <Boton texto='4' clase='numero' />
+        <Boton texto='5' clase='numero' />
+        <Boton texto='6' clase='numero' />
+        <Boton texto='-' clase='operacion' />
+        <Boton texto='1' clase='numero' />
+        <Boton texto='2' clase='numero' />
+        <Boton texto='3' clase='numero' />
+        <Boton texto='+' clase='operacion' />
+        <Boton texto='0' clase='cero' />
+        <Boton texto='.' clase='numero' />
+        <Boton texto='=' clase='operacion' />
     </main>
   )
 }
