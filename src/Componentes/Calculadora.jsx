2d0e4a29b40594ac5a9a835b3f2d2c6b7a7e7cc7
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-05-18 20:07:27 -0600

    Styles and jsx creation

---
+++ src/Componentes/Calculadora.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './calculadora.css'
+
+const Calculadora = () => {
+  return (
+    <main>
+        <input className='answer' type="text" />
+    </main>
+  )
+}
+
+export default Calculadora
